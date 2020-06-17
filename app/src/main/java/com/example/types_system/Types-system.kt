d40a49f14ffed2385febefcc740741a41ea223ab
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-17 09:22:42 -0400

    Make a func to navigate a another place

--- app/src/main/java/com/example/types_system/Types-system.kt
+++ app/src/main/java/com/example/types_system/Types-system.kt
@@ -11,6 +11,20 @@ class Sunny: Weather("☀️")
 
 class MyEvilWeather: Weather("😈")
 
+fun navigateToNextScreen(weather: Weather) {
+
+    if (weather is Rainy) {
+        println("go to buy an umbrella")
+    }
+    if (weather is Windy) {
+        println("go to buy a coffe")
+    }
+    if (weather is Sunny) {
+        println("go to buy an ice cream")
+    }
+}
+
 fun main(args: Array<String>) {
-    print(Rainy().label)
+//    print(Rainy().label)
+    navigateToNextScreen(Rainy())
 }
